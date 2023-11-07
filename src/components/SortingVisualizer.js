14f80f24dfe2041547427fd4b205e58d699784f6
Akos <benczikakos@gmail.com>
2023-11-07 17:18:58 +0100

    configurable colors

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -2,11 +2,25 @@ import {useState} from 'react';
 import Stack from '@mui/material/Stack';
 import { BarChart } from '@mui/x-charts';
 import Button  from '@mui/material/Button';
+import Slider from '@mui/material/Slider';
 
 
 function SortingVisualizer({length = 25}) {
+    //utils
+    function makeRandomArray() {
+        const randomArray = Array.from({ length }, () => Math.floor(Math.random() * 100 + 100));
+        return randomArray;
+    }
+
+    function mapValuesToObjectArray(){
+        const arrayOfObjects = values.map((element) => ({ data: element }));
+        return arrayOfObjects
+    }
+    //states
     const [values, setValues] = useState(() => makeRandomArray());
-    const handleClick = (event) => {
+    const [colors, setColors] = useState(Array(length).fill('#fac420'));
+    //click handlers
+    const handleShuffleClick = () => {
         const shuffledArray = [...values]; // Clone the input array to avoid modifying the original.
         for (let i = shuffledArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -15,24 +29,37 @@ function SortingVisualizer({length = 25}) {
         setValues(shuffledArray);
     }
 
-    function makeRandomArray() {
-        const randomArray = Array.from({ length }, () => Math.floor(Math.random() * 100 + 100));
-        return randomArray;
+    const handleResetClick = () => {
+        setValues(makeRandomArray());
     }
 
+    const handleSliderChange = (event, newValue) => {
+        const yellow = Array(newValue).fill('#fac420');
+        const grey = Array(length - newValue).fill('#d3d3d3');
+        setColors([...yellow, ...grey]);
+    }
+
+    const seriesData = values.map((element) => ({ data: element }));
     return (
         <Stack direction="row" spacing={2} alignItems='center' justifyContent='center' mr={2}>
             <BarChart
-                series={[{ data: values }]}
+                series={values.map((s) => ({ data: [s] }))}
                 // width={500}
                 height={500}
                 bottomAxis={null}
                 leftAxis={null}
-                colors={Array(7).fill('#fac430')}
+                colors={colors}
             />
             <Stack mr={2} spacing={2}>
-                <Button variant="contained" sx={'max-height:50px'} onClick={handleClick}>Shuffle</Button>
-                <Button variant="contained" sx={'max-height:50px'} onClick={() => setValues(makeRandomArray())}>Reset</Button>
+                <Button variant="contained" sx={'max-height:50px'} onClick={handleShuffleClick}>Shuffle</Button>
+                <Button variant="contained" sx={'max-height:50px'} onClick={handleResetClick}>Reset</Button>
+                <Slider
+                    min={0}
+                    max={length}
+                    step={1}
+                    defaultValue={length}
+                    onChange={handleSliderChange}
+                />
             </Stack>
         </Stack> 
     );
