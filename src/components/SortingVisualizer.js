c3be70dbd590d32f93f0f0bc1f76cd6f8265245d
Akos <benczikakos@gmail.com>
2023-11-07 19:06:55 +0100

    active added but works strange

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -5,23 +5,31 @@ import Button  from '@mui/material/Button';
 import Slider from '@mui/material/Slider';
 
 
-function SortingVisualizer({length = 25}) {
+function SortingVisualizer({length = 5}) {
     //utils
     function makeRandomArray() {
-        const randomArray = Array.from({ length }, () => Math.floor(Math.random() * 100 + 100));
+        const randomArray = Array.from({ length }, () => Math.floor(Math.random() * 100 + 10));
         return randomArray;
     }
 
-    function mapValuesToObjectArray(){
-        const arrayOfObjects = values.map((element) => ({ data: element }));
-        return arrayOfObjects
+    function recalculateColors() {
+        const yellow = Array(numYellow).fill('#fac420');
+        const grey = Array(length - numYellow).fill('#d3d3d3');
+        console.log(numYellow, activeIndex);
+        let newColors = [...yellow, ...grey];
+        if(activeIndex !== -1){
+            newColors[activeIndex] = '#E0039C';
+        }
+        setColors(newColors);
     }
     //states
     const [values, setValues] = useState(() => makeRandomArray());
     const [colors, setColors] = useState(Array(length).fill('#fac420'));
+    const [numYellow, setNumYellow] = useState(length-1); //for slider [0, length]
+    const [activeIndex, setActiveIndex] = useState(-1);
     //click handlers
     const handleShuffleClick = () => {
-        const shuffledArray = [...values]; // Clone the input array to avoid modifying the original.
+        const shuffledArray = [...values];
         for (let i = shuffledArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
@@ -34,31 +42,43 @@ function SortingVisualizer({length = 25}) {
     }
 
     const handleSliderChange = (event, newValue) => {
-        const yellow = Array(newValue).fill('#fac420');
-        const grey = Array(length - newValue).fill('#d3d3d3');
-        setColors([...yellow, ...grey]);
+        setNumYellow(newValue);
+        recalculateColors();
     }
 
-    const seriesData = values.map((element) => ({ data: element }));
+    const handleSecondSliderChange = (event, newValue) => {
+        setActiveIndex(newValue);
+        recalculateColors();
+    }   
+
     return (
         <Stack direction="row" spacing={2} alignItems='center' justifyContent='center' mr={2}>
             <BarChart
                 series={values.map((s) => ({ data: [s] }))}
-                // width={500}
                 height={500}
                 bottomAxis={null}
                 leftAxis={null}
                 colors={colors}
+                axisHighlight={{x: 'none', y: 'none'}}
             />
             <Stack mr={2} spacing={2}>
                 <Button variant="contained" sx={'max-height:50px'} onClick={handleShuffleClick}>Shuffle</Button>
                 <Button variant="contained" sx={'max-height:50px'} onClick={handleResetClick}>Reset</Button>
                 <Slider
                     min={0}
-                    max={length}
+                    max={length-1}
                     step={1}
                     defaultValue={length}
                     onChange={handleSliderChange}
+                    valueLabelDisplay="on"
+                />
+                <Slider
+                    min={-1}
+                    max={length-1}
+                    step={1}
+                    defaultValue={-1}
+                    onChange={handleSecondSliderChange}
+                    valueLabelDisplay="on"
                 />
             </Stack>
         </Stack> 
