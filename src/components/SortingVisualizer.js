fc6bf705808815ccd6b5505c64ef189409a874da
Akos <benczikakos@gmail.com>
2023-11-08 14:06:04 +0100

    algoSelecter added

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -3,9 +3,13 @@ import Stack from '@mui/material/Stack';
 import { BarChart } from '@mui/x-charts';
 import Button  from '@mui/material/Button';
 import Slider from '@mui/material/Slider';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 
-function SortingVisualizer({length = 10}) {
+function SortingVisualizer({length = 20}) {
     //utils
     function makeRandomArray() {
         const randomArray = Array.from({ length }, () => Math.floor(Math.random() * 100 + 10));
@@ -15,7 +19,6 @@ function SortingVisualizer({length = 10}) {
     function RecalculateColors() {
         const yellow = Array(numYellow).fill('#fac420');
         const grey = Array(length - numYellow).fill('#d3d3d3');
-        console.log(numYellow, activeIndex);
         let newColors = [...yellow, ...grey];
         if(activeIndex !== -1){
             newColors[activeIndex] = '#E0039C';
@@ -27,6 +30,7 @@ function SortingVisualizer({length = 10}) {
     const [colors, setColors] = useState(Array(length).fill('#fac420'));
     const [numYellow, setNumYellow] = useState(length); //for slider [0, length]
     const [activeIndex, setActiveIndex] = useState(-1);
+    const [algorithm, setAlgorithm] = useState('bubbleSort'); //for dropdown
     //hooks
     useEffect(() => RecalculateColors(), [numYellow]);
     useEffect(() => RecalculateColors(), [activeIndex]);
@@ -50,7 +54,11 @@ function SortingVisualizer({length = 10}) {
 
     const handleSecondSliderChange = (_, newValue) => {
         setActiveIndex(newValue);
-    }   
+    }
+    
+    const handleAlgorithmChange = (event) => {
+        setAlgorithm(event.target.value);
+    }
 
     return (
         <Stack direction="row" spacing={2} alignItems='center' justifyContent='center' mr={2}>
@@ -71,7 +79,7 @@ function SortingVisualizer({length = 10}) {
                     step={1}
                     defaultValue={length}
                     onChange={handleSliderChange}
-                    valueLabelDisplay="on"
+                    sx={{marginRight: '10px'}}
                 />
                 <Slider
                     min={-1}
@@ -79,8 +87,21 @@ function SortingVisualizer({length = 10}) {
                     step={1}
                     defaultValue={-1}
                     onChange={handleSecondSliderChange}
-                    valueLabelDisplay="on"
                 />
+                <FormControl fullWidth>
+                    <InputLabel id="algorithm-select-label">Algorithm</InputLabel>
+                    <Select
+                      labelId="algorithm-select-label"
+                      id="algorithm-select"
+                      value={algorithm}
+                      label="Algorithm"
+                      onChange={handleAlgorithmChange}
+                    >
+                      <MenuItem value={'bubbleSort'}>Bubble sort</MenuItem>
+                      <MenuItem value={'mergeSort'}>Merge sort</MenuItem>
+                      <MenuItem value={'quickSort'}>Quick sort</MenuItem>
+                    </Select>
+                </FormControl>
             </Stack>
         </Stack> 
     );
