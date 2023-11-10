0f33b0c71a30f2647aa6f5354ea06db5e2687c6b
Akos <benczikakos@gmail.com>
2023-11-10 18:36:56 +0100

    nicer insertionSort

--- src/components/Sorting/algorithms/insertionSort.js
+++ src/components/Sorting/algorithms/insertionSort.js
@@ -4,26 +4,28 @@ function delay(milliseconds) {
     });
 }
 
-async function InsertionSort(arr, arraySetter,numberOfSortedSetter, highlightOne, sleepTime) {
+async function InsertionSort(arr, arraySetter, numberOfSortedSetter, highlightOne, sleepTime) {
     const len = arr.length;
     
     for (let i = 1; i < len; i++) {
+        numberOfSortedSetter(i);
         const key = arr[i];
         let j = i - 1;
         
         while (j >= 0 && arr[j] > key) {
-            highlightOne(j + 1);
+            highlightOne(j);
             await delay(sleepTime);
 
             arr[j + 1] = arr[j];
-            arraySetter([...arr]);
-
             j--;
         }
         
         arr[j + 1] = key;
         arraySetter([...arr]);
+        await delay(sleepTime);
     }
+    numberOfSortedSetter(len);
+    highlightOne(-1);
 }
 
 export default InsertionSort;
