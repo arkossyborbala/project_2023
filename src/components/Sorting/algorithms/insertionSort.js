3d44c787a9ce18c58b38e9dcc079a59324df4eb2
Akos <benczikakos@gmail.com>
2023-11-28 16:21:02 +0100

    optimized insertion sort

--- src/components/Sorting/algorithms/insertionSort.js
+++ src/components/Sorting/algorithms/insertionSort.js
@@ -2,25 +2,39 @@ import {delay} from '../../../App.js'
 
 async function InsertionSort(arr, arraySetter, numberOfSortedSetter, highlightOne, highlightTwo, sleepTime) {
     const len = arr.length;
-    
+
     for (let i = 1; i < len; i++) {
         highlightTwo(i);
         numberOfSortedSetter(i);
+
         const key = arr[i];
-        let j = i - 1;
-        
-        while (j >= 0 && arr[j] > key) {
-            highlightOne(j);
+        let left = 0;
+        let right = i - 1;
+
+        // Binary search for the correct position of the current element
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            highlightOne(mid);
             await delay(sleepTime);
 
-            arr[j + 1] = arr[j];
-            j--;
+            if (arr[mid] > key) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        
-        arr[j + 1] = key;
+
+        // Shift elements to make space for the current element
+        for (let j = i - 1; j >= left; j--) {
+            arr[j + 1] = arr[j];
+        }
+
+        // Insert the current element at the correct position
+        arr[left] = key;
         arraySetter([...arr]);
         await delay(sleepTime);
     }
+
     numberOfSortedSetter(len);
     highlightOne(-1);
     highlightTwo(-1);
